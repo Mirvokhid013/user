d13f2ce7bd2a84a1eb06f9a8bb25b17236ca66dc
Mirvokhid013 <mirvokhid013@gmail.com>
2024-02-19 19:37:07 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let root = document.getElementById("root")
+
+console.log(root);
+
+root.setAttribute("class", 'text-center')
+
+root.style.marginTop = '200px'
+
+let elForm = document.createElement("form");
+let elInput = document.createElement("input");
+let btn = document.createElement("button");
+let value = document.createElement("h1");
+
+btn.textContent = 'Submit'
+
+elForm.append(elInput, btn);
+
+root.append(elForm, value);
+
+value.style.fontSize = '200px'
+
+elForm.addEventListener("submit", (evt)=> {
+    evt.preventDefault();
+    elForm.style.display = 'none';
+
+    value.innerHTML = `I ❤️ ${elInput.value}`;
+    elInput.value = '';
+})
